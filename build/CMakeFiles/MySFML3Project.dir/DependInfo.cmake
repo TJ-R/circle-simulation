73d45f15a2ebd12a9581ba84bc914380efafdb93
TJ <runyant13@gmail.com>
2025-12-04 17:54:32 -0500

    Changed dependency path for windows to not conflict with linux cmake build

---
+++ build/CMakeFiles/MySFML3Project.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/tj/workspace/github.com/TJ-R/circle-simulation/circle-simulation/ball.cpp" "CMakeFiles/MySFML3Project.dir/circle-simulation/ball.cpp.o" "gcc" "CMakeFiles/MySFML3Project.dir/circle-simulation/ball.cpp.o.d"
+  "/home/tj/workspace/github.com/TJ-R/circle-simulation/circle-simulation/main.cpp" "CMakeFiles/MySFML3Project.dir/circle-simulation/main.cpp.o" "gcc" "CMakeFiles/MySFML3Project.dir/circle-simulation/main.cpp.o.d"
+  "" "MySFML3Project" "gcc" "CMakeFiles/MySFML3Project.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/circle-simulation.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/circle-simulation.dir/link.d"
+  "CMakeFiles/circle-simulation.dir/circle-simulation/ball.cpp.o"
+  "CMakeFiles/circle-simulation.dir/circle-simulation/ball.cpp.o.d"
+  "CMakeFiles/circle-simulation.dir/circle-simulation/main.cpp.o"
+  "CMakeFiles/circle-simulation.dir/circle-simulation/main.cpp.o.d"
+  "circle-simulation"
+  "circle-simulation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/circle-simulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
